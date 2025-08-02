d603d9daed6a01c9ac4addd0556a235be5620cfd
motconkien <hynuiux@gmail.com>
2025-08-02 13:58:07 +0900

    updated

--- Frontend/dashboard/src/pages/Price.js
+++ Frontend/dashboard/src/pages/Price.js
@@ -21,39 +21,44 @@ function Dashboard() {
     }, []);
     return (
         <div className="price-page">
+            <div className="stick-container">
+                <h1>LPs Price</h1>
+            </div>
             <div className="table-container">
-                <table className="price-table">
-                    <thead>
-                        <tr>
-                            <th style={{ width: '50%' }}>Account</th>
-                            <th style={{ width: '50%' }}>Symbol</th>
-                            <th style={{ width: '50%' }}>Bid</th>
-                            <th style={{ width: '50%' }}>Ask</th>
-                            <th style={{ width: '50%' }}>Spread</th>
-                            <th style={{ width: '50%' }}>Swap long</th>
-                            <th style={{ width: '50%' }}>Swap short</th>
-                        </tr>
-                    </thead>
+                {Object.keys(priceData).length > 0 ? (
+                    <table className="price-table">
+                        <thead>
+                            <tr>
+                                <th style={{ width: '50%' }}>Account</th>
+                                <th style={{ width: '50%' }}>Symbol</th>
+                                <th style={{ width: '50%' }}>Bid</th>
+                                <th style={{ width: '50%' }}>Ask</th>
+                                <th style={{ width: '50%' }}>Spread</th>
+                                <th style={{ width: '50%' }}>Swap long</th>
+                                <th style={{ width: '50%' }}>Swap short</th>
+                            </tr>
+                        </thead>
 
-                    <tbody>
-                        {priceData &&
-                            Object.entries(priceData).flatMap(([account, symbols]) => (
-                                // console.log(priceData)
-                                Object.entries(symbols).map(([symbol, tick]) => (
-                                    <tr key={`${account}-${symbol}`}>
-                                        <td>{account}</td>
-                                        <td>{symbol}</td>
-                                        <td>{tick.bid}</td>
-                                        <td>{tick.ask}</td>
-                                        <td>{tick.spread}</td>
-                                        <td>{tick.swap_long}</td>
-                                        <td>{tick.swap_short}</td>
-                                    </tr>
-                                ))
-                            ))}
-                    </tbody>
-                </table>
-
+                        <tbody>
+                            {/* flatten data by objec.entries to return the list of list */}
+                            {priceData &&
+                                Object.entries(priceData).flatMap(([account, symbols]) => (
+                                    // console.log(priceData)
+                                    Object.entries(symbols).map(([symbol, tick]) => (
+                                        <tr key={`${account}-${symbol}`}>
+                                            <td>{account}</td>
+                                            <td>{symbol}</td>
+                                            <td>{tick.bid}</td>
+                                            <td>{tick.ask}</td>
+                                            <td>{tick.spread}</td>
+                                            <td>{tick.swap_long}</td>
+                                            <td>{tick.swap_short}</td>
+                                        </tr>
+                                    ))
+                                ))}
+                        </tbody>
+                    </table>
+                ) : (<p>No data available</p>)}
             </div>
 
         </div>
